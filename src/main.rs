da806c6581b3416abbb825f3427586c5b5fe994b
Hongchao Liu <lbs.lhc@gmail.com>
2019-08-08 01:18:50 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,15 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
-
-extern crate reqwest;
-extern crate serde;
-extern crate chrono;
-extern crate crossbeam;
-
 #[macro_use] extern crate prometheus;
 #[macro_use] extern crate lazy_static;
 
-use serde::{Serialize, Deserialize};
-use chrono::{DateTime, Utc};
-use prometheus::{IntGauge, TextEncoder, Encoder};
-use log::info;
 use std::time::Duration;
 
 pub mod schedule;
-
-lazy_static! {
-    static ref UNCONFIRMED_COUNT_GAUGE: IntGauge = register_int_gauge!(
-        "unconfirmed_count_gauge",
-        "Number of unconfirmed transaction in memory pool"
-    ).unwrap();
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct BlockCypherBitcoinChainResponse {
-    name: String,
-    height: u32,
-    hash: String,
-    time: DateTime<Utc>,
-    latest_url: String,
-    previous_hash: String,
-    previous_url: String,
-    unconfirmed_count: u32,
-    high_fee_per_kb: u32,
-    medium_fee_per_kb: u32,
-    low_fee_per_kb: u32,
-    last_fork_height: u32
-}
-
-#[get("/")]
-fn index() -> String {
-    // Gather the metrics.
-    let mut buffer = vec![];
-    let encoder = TextEncoder::new();
-    let metric_families = prometheus::gather();
-    encoder.encode(&metric_families, &mut buffer).unwrap();
-
-    let result = String::from_utf8(buffer).unwrap();
-    return result;
-}
+pub mod prometheus_metrics;
 
 fn main() {
-    schedule::repeat(Duration::from_secs(60), || {
-        let body: BlockCypherBitcoinChainResponse = reqwest::get("https://api.blockcypher.com/v1/btc/main")
-            .unwrap()
-            .json()
-            .unwrap();
-
-        info!("{:?}", body);
-        UNCONFIRMED_COUNT_GAUGE.set(body.unconfirmed_count as i64);
-    });
-
-    rocket::ignite().mount("/", routes![index]).launch();
+    schedule::repeat(Duration::from_secs(60), prometheus_metrics::set_metrics);
+    rocket::ignite().mount("/", routes![prometheus_metrics::endpoint]).launch();
 }
---
+++ src/prometheus_metrics.rs
@@ -0,0 +1,49 @@
+use log::info;
+use prometheus::{TextEncoder, Encoder, IntGauge};
+use serde::{Serialize, Deserialize};
+use chrono::{DateTime, Utc};
+
+lazy_static! {
+    pub static ref UNCONFIRMED_COUNT_GAUGE: IntGauge = register_int_gauge!(
+        "unconfirmed_count_gauge",
+        "Number of unconfirmed transaction in memory pool"
+    ).unwrap();
+}
+
+#[get("/metrics")]
+pub fn endpoint() -> String {
+    // Gather metrics from the default registry.
+    let mut buffer = vec![];
+    let encoder = TextEncoder::new();
+    let metric_families = prometheus::gather();
+    encoder.encode(&metric_families, &mut buffer).unwrap();
+
+    let result = String::from_utf8(buffer).unwrap();
+    return result;
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct BlockCypherBitcoinChainResponse {
+    pub name: String,
+    pub height: u32,
+    pub hash: String,
+    pub time: DateTime<Utc>,
+    pub latest_url: String,
+    pub previous_hash: String,
+    pub previous_url: String,
+    pub unconfirmed_count: u32,
+    pub high_fee_per_kb: u32,
+    pub medium_fee_per_kb: u32,
+    pub low_fee_per_kb: u32,
+    pub last_fork_height: u32
+}
+
+pub fn set_metrics() -> () {
+    let body: BlockCypherBitcoinChainResponse = reqwest::get("https://api.blockcypher.com/v1/btc/main")
+        .unwrap()
+        .json()
+        .unwrap();
+
+    info!("{:?}", body);
+    UNCONFIRMED_COUNT_GAUGE.set(body.unconfirmed_count as i64);
+}
